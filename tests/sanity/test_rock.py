4f670751906175c1743d17fe9d033436a5b6441b
Benjamin Schimke <benjamin.schimke@canonical.com>
2025-11-06 17:56:37 +0100

    fix test

--- tests/sanity/test_rock.py
+++ tests/sanity/test_rock.py
@@ -14,7 +14,7 @@ IMAGE_BASE = f"ghcr.io/canonical/{IMAGE_NAME}"
 IMAGE_ENTRYPOINT = "/metrics-server --version"
 
 
-@pytest.mark.parametrize("image_version", env_util.image_versions_in_repo(REPO_PATH))
+@pytest.mark.parametrize("image_version", env_util.image_versions_in_repo(IMAGE_NAME, REPO_PATH))
 def test_executable(image_version):
     image = env_util.resolve_image(IMAGE_NAME, image_version)
     docker_util.run_entrypoint_and_assert(
@@ -22,16 +22,16 @@ def test_executable(image_version):
     )
 
 
-@pytest.mark.parametrize("image_version", env_util.image_versions_in_repo(REPO_PATH))
+@pytest.mark.parametrize("image_version", env_util.image_versions_in_repo(IMAGE_NAME, REPO_PATH))
 def test_pebble_executable(image_version):
     image = env_util.resolve_image(IMAGE_NAME, image_version)
     docker_util.run_entrypoint_and_assert(
-        image, "/bin/pebble version", expect_stdout_contains="v1.14.0"
+        image, "/bin/pebble version", expect_stdout_contains="v1.18.0"
     )
 
 
 @pytest.mark.parametrize("GOFIPS", [0, 1], ids=lambda v: f"GOFIPS={v}")
-@pytest.mark.parametrize("image_version", env_util.image_versions_in_repo(REPO_PATH))
+@pytest.mark.parametrize("image_version", env_util.image_versions_in_repo(IMAGE_NAME, REPO_PATH))
 def test_fips(image_version, GOFIPS):
     image = env_util.resolve_image(IMAGE_NAME, image_version)
     entrypoint = shlex.split(IMAGE_ENTRYPOINT)
